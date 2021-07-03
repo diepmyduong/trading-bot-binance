6ba951e9db37f691ddc7dc745182b4650f1da2b5
diepmyduong@outlook.com.vn <diepmyduong@outlook.com.vn>
2021-07-03 11:08:03 +0700

    Fix: Buy BTC

--- bot2.js
+++ bot2.js
@@ -108,7 +108,8 @@ Time Frame: ${this.tfLong} : ${this.tfShort}`);
             const cond2 = preBar.open < smaShort1 && preBar.close > smaShort1;
             const cond3 = preBar.low > preSar1;
             const cond5 = preBar && preBar.close <= smaShort1 * 1.05;
-            const cond6 = require("./btc_change.json").change_24h > 1;
+            const btcChange = require("./btc_change.json");
+            const cond6 = btcChange.change_24h > 1 && btcChange.change_1h > 1;
             if (cond1 & cond2 && cond3 && cond5 && cond6) {
               await this.buy(barLong, smaLong, preBar, smaShort1, barShort);
             }
